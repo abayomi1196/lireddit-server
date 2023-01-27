c2d0420b00942e2983804e64e6153a3a2a9b4fde
Abayomi Ayodele <samabayomi301@gmail.com>
2023-01-27 07:20:32 +0100

    setup graphql resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-import { Post } from "./entitites/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
 
   orm.getMigrator().up();
 
-  // const post = orm.em.create(Post, {
-  //   title: "fist post",
-  //   createdAt: new Date(),
-  //   updatedAt: new Date()
-  // });
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
 
-  // await orm.em.persistAndFlush(post);
+  const { url } = await startStandaloneServer(apolloServer, {
+    listen: { port: 4000 }
+  });
 
-  // const posts = await orm.em.find(Post, {});
-
-  // console.log(posts);
+  console.log(`🚀 Server ready at: ${url}`);
 };
 
 main().catch((err) => console.error(err));
