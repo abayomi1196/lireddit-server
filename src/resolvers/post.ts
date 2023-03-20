b662ee25b3a2ebac973d5871b87cb0e2a926fa44
Abayomi Ayodele <samabayomi301@gmail.com>
2023-03-20 21:36:00 +0100

    setup basic auth middleware for validating post creation

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,25 @@
-import { Resolver, Query, Arg, Mutation } from "type-graphql";
+import { isAuth } from "../middleware/isAuth";
+import { MyContext } from "../types";
+import {
+  Resolver,
+  Query,
+  Arg,
+  Mutation,
+  Field,
+  InputType,
+  Ctx,
+  UseMiddleware
+} from "type-graphql";
 
 import { Post } from "../entities/Post";
+
+@InputType()
+class PostInput {
+  @Field()
+  title: string;
+  @Field()
+  text: string;
+}
 
 @Resolver()
 export class PostResolver {
@@ -18,8 +37,15 @@ export class PostResolver {
 
   // create post
   @Mutation(() => Post)
-  async createPost(@Arg("title") title: string): Promise<Post> {
-    return Post.create({ title }).save();
+  @UseMiddleware(isAuth)
+  async createPost(
+    @Arg("input") input: PostInput,
+    @Ctx() { req }: MyContext
+  ): Promise<Post> {
+    return Post.create({
+      ...input,
+      creatorId: req.session.userId
+    }).save();
   }
 
   // update post
